e88282eb9e0f849125520024aa3f19ff27d4c575
Jorge Martín <je.martin@uniandes.edu.co>
2018-10-01 22:13:13 -0500

    [Rojo] - Iteración 3 Ciclo 4, string con cadena n elemento y devuelve número de elementos,  mínimo de la lista y máximo

--- test_stats.py
+++ test_stats.py
@@ -14,9 +14,9 @@ class TestStats(TestCase):
         result = stats.calc_stats("1,10")
         a = [2, 1, 10]
         self.assertTrue(result == a, msg="Cuando hay 2 elemento en el string")
-        # result = stats.calc_stats("1,10,8,4,12")
-        # a = [5, 1]
-        # self.assertTrue(result == a, msg="Cuando hay n elemento en el string")
+        result = stats.calc_stats("1,10,8,4,12")
+        a = [5, 1, 12]
+        self.assertTrue(result == a, msg="Cuando hay n elemento en el string")
 
 
 

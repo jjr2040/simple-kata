04df970c1575feef8a1900d2b87857639f84931b
Juan Jose Villegas <jj.villegas47@gmail.com>
2018-10-01 22:06:50 -0500

    [Verde]: Iteracion 3 Ciclo 1  -> Prueba con string vacía pasa las prueba. Devuelve el número de elementos, el mínimo y el máximo

--- stats.py
+++ stats.py
@@ -2,11 +2,13 @@
 class Stats:
     def calc_stats(self, entrada):
         num_elements = self.num_elements(entrada)
-
         min_elements = self.min_elements(entrada)
+        max_elements = self.max_elements(entrada)
+
         resp = [
             num_elements,
-            min_elements
+            min_elements,
+            max_elements
         ]
 
         return resp
@@ -16,3 +18,6 @@ class Stats:
 
     def min_elements(self, string):
         return int(min(string.split(','))) if string.strip() is not '' else 0
+
+    def max_elements(self, string):
+        return 0

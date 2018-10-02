af91f4be937dda9535a2d05d936d7a8788771008
Juan Jose Villegas <jj.villegas47@gmail.com>
2018-10-01 21:14:55 -0500

    [Verde]: Iteracion 1 Ciclo 3  -> Pasa la prueba de devolver el numero de elementos del string

--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 
 class Stats:
     def calc_stats(self, entrada):
-        num = entrada.split(',').count
-        return [len(num)]
+        num_elements = entrada.split(',')
+        return [len(num_elements)]
 

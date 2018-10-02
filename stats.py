11246358993920dd50cc1c71df1e8897177e3a4c
Juan Jose Villegas <jj.villegas47@gmail.com>
2018-10-01 21:43:57 -0500

    [Verde]: Iteracion 2 Ciclo 1  -> Prueba con string vacio, pasa

--- stats.py
+++ stats.py
@@ -3,11 +3,21 @@ class Stats:
     def calc_stats(self, entrada):
         num_elements = self.num_elements(entrada)
 
-        resp = [
-            num_elements
-        ]
+        if num_elements != 0:
+            resp = [
+                num_elements
+            ]
+        else:
+            min_elements = self.min_elements(entrada)
+            resp = [
+                num_elements,
+                min_elements
+            ]
 
         return resp
 
     def num_elements(self, string):
         return len(string.split(',')) if string.strip() is not '' else 0
+
+    def min_elements(self, string):
+        return 0

d1f2698177f2cc5f25c68a151610da01e492d545
Juan Jose Villegas <jj.villegas47@gmail.com>
2018-10-01 22:25:39 -0500

    [Verde]: Iteracion 4 Ciclo 1  -> Prueba con string con cadena vacía pasa las prueba. Devuelve el número de elementos, el mínimo, el máximo y el promedio

--- stats.py
+++ stats.py
@@ -4,11 +4,13 @@ class Stats:
         num_elements = self.num_elements(entrada)
         min_elements = self.min_elements(entrada)
         max_elements = self.max_elements(entrada)
+        avg_elements = self.avg_elements(entrada)
 
         resp = [
             num_elements,
             min_elements,
-            max_elements
+            max_elements,
+            avg_elements
         ]
 
         return resp
@@ -27,6 +29,9 @@ class Stats:
             return 0
         else:
             return max(self.string_array_to_int_array(string))
+
+    def avg_elements(self, string):
+        return 0
 
     def string_array_to_int_array(self, string):
         return [int(x) for x in string.split(',')]

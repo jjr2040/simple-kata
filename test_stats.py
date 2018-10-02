0c64aeb51c744a92058d34b585db20f2235c624c
Jorge Martín <je.martin@uniandes.edu.co>
2018-10-01 21:01:54 -0500

    [Rojo] - Iteración 1 Ciclo 2, string con un número de entrada y devuelve cantidad de elementos

--- test_stats.py
+++ test_stats.py
@@ -5,7 +5,8 @@ from stats import Stats
 class TestStats(TestCase):
     def test_calc_stats(self):
         stats = Stats()
-        result = stats.calc_stats("")
-        a = [0]
+        result = stats.calc_stats("1")
+        a = [1]
         self.assertTrue(result == a)
+
 

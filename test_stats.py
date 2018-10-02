5486cc8d99e58b030485e3b584fa3bd084f12d75
Jorge Martín <je.martin@uniandes.edu.co>
2018-10-01 21:29:19 -0500

    [Rojo] - Iteración 1 Ciclo 4, string con n números de entrada y devuelve cantidad de elementos, validando que cuando el string está vacío no se daña

--- test_stats.py
+++ test_stats.py
@@ -5,8 +5,17 @@ from stats import Stats
 class TestStats(TestCase):
     def test_calc_stats(self):
         stats = Stats()
+        result = stats.calc_stats("")
+        a = [0]
+        self.assertTrue(result == a, msg="Cuando string llega vacio")
+        result = stats.calc_stats("1")
+        a = [1]
+        self.assertTrue(result == a,  msg="Cuando hay 1 elemento en el string")
+        result = stats.calc_stats("1,10")
+        a = [2]
+        self.assertTrue(result == a, msg="Cuando hay 2 elemento en el string")
         result = stats.calc_stats("1,10,8,4,12")
         a = [5]
-        self.assertTrue(result == a)
+        self.assertTrue(result == a, msg="Cuando hay n elemento en el string")
 
 
